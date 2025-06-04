82eccbdc87c14292eff4c3a1d3d4a6c4d34b0197
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2025-06-04 09:16:40 -0700

    feat: adds functions to service layer

--- src/main/java/com/example/MarketAPI/model/Product.java
+++ src/main/java/com/example/MarketAPI/model/Product.java
@@ -2,12 +2,15 @@ package com.example.MarketAPI.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/example/MarketAPI/service/ProductService.java
+++ src/main/java/com/example/MarketAPI/service/ProductService.java
@@ -1,7 +1,34 @@
 package com.example.MarketAPI.service;
 
+import com.example.MarketAPI.model.Product;
+import com.example.MarketAPI.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAll(){
+        return repository.findAll();
+    }
+
+    public Product create(Product product){
+        return repository.save(product);
+    }
+
+    public Product update(Long id, Product product){
+        product.setId(id);
+        return repository.save(product);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
