d118e3b679cc52c892aa5dcc7b66e89b57148412
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2025-06-05 09:01:35 -0700

    refactor: adds validations to model with hibernate.validation

--- src/main/java/com/example/MarketAPI/model/Product.java
+++ src/main/java/com/example/MarketAPI/model/Product.java
@@ -1,9 +1,13 @@
 package com.example.MarketAPI.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
@@ -16,9 +20,19 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "product_img")
+    @URL
     private String img;
+    @Column(name="product_name")
+    @NotBlank
     private String name;
+    @Column(name = "product_price")
+    @Positive
     private double price;
+    @Column(name = "product_stock")
+    @Min(0)
     private int stock;
+    @Column(name = "product_brand")
+    @NotBlank
     private String brand;
 }
